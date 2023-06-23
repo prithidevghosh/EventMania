85dd46b9c40d5c81abbcb5b93b3b4a8db87bf596
Prithidev Ghosh <Prithidevghosh@gmail.com>
2023-06-23 07:28:35 +0530

    changes

--- controller/event-controller.js
+++ controller/event-controller.js
@@ -65,6 +65,33 @@ module.exports.handleRegistration = async (req, res) => {
     }
 }
 
+module.exports.handleDeRegistration = async (req, res) => {
+    try {
+        const eventFetchedDb = await Event.findById(req.params.id1);
+        if (eventFetchedDb) {
+            const num = eventFetchedDb.participants;
+            eventFetchedDb.participants = num - 1;
+            const finalRes = await eventFetchedDb.save();
+
+            const userFetchedDb = await User.findById(req.params.id2);
+            console.log(userFetchedDb.id);
+            console.log("users db", eventFetchedDb.users);
+
+            const index = eventFetchedDb.users.findIndex(userId => userId === userFetchedDb.id);
+            if (index !== -1) {
+                eventFetchedDb.users.splice(index, 1);
+                await eventFetchedDb.save();
+            }
+        }
+        return res.redirect('back');
+    } catch (error) {
+        console.log(error);
+       
+        res.status(500).json({ error: 'An error occurred' });
+    }
+};
+
+
 module.exports.deleteEvent = async (req, res) => {
     try {
 
--- routes/events.js
+++ routes/events.js
@@ -8,4 +8,5 @@ router.post('/create', eventcontroller.create)
 router.get('/filter/:id', eventcontroller.filter)
 router.get('/register/:id1/:id2', eventcontroller.handleRegistration)
 router.get('/delete/:id', eventcontroller.deleteEvent)
+router.get('/deregister/:id1/:id2', eventcontroller.handleDeRegistration);
 module.exports = router;
